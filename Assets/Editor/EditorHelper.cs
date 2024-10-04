85f0c138ee58e32f0927b22dca5da26bb0b9c6aa
dingzexing <burcetw@gmail.com>
2024-10-04 14:34:15 +0800

    启动项调整

---
+++ Assets/Editor/EditorHelper.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEditor;
+using UnityEngine;
+
+public class EditorHelper : EditorWindow
+{
+    [MenuItem("Helper/RunGame %#&p")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        //EditorHelper window = (EditorHelper)EditorWindow.GetWindow(typeof(EditorHelper));
+        string curScene = EditorSceneManager.GetActiveScene().name;
+        //EditorApplication.SaveScene();
+        EditorSceneManager.OpenScene("Assets/Launcher.unity");
+        EditorApplication.isPlaying = true;
+        UnityEngine.Debug.Log(curScene);
+    }
+    [MenuItem("Helper/Open C# Project")]
+    static void OpenCSharpProject()
+    {
+        string path = "Assets/GameMain/Scripts/Base/GameEntry.cs";
+        UnityEngine.Object obj = AssetDatabase.LoadAssetAtPath(path, typeof(MonoScript));
+        AssetDatabase.OpenAsset(obj, 0);
+    }
+}
