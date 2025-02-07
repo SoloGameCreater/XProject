491aac63bbac023238b40326e6e14afe25e4cbab
dingzexing <burcetw@gmail.com>
2025-02-07 22:10:25 +0800

    部分代码整理

--- XProject/Assets/GameMain/Scripts/TripleMerge/MergeObject/InEditor/MergeableCell.Editor.cs
+++ XProject/Assets/GameMain/Scripts/TripleMerge/MergeObject/InEditor/MergeableCell.Editor.cs
@@ -9,6 +9,7 @@ namespace TripleMerge
 {
     public partial class MergeableCell
     {
+#if UNITY_EDITOR
         private readonly ValueDropdownList<int> _mergeableItemList = new();
         private ValueDropdownList<int> GetMergeableItemConfigList()
         {
@@ -26,5 +27,23 @@ namespace TripleMerge
 
             return _mergeableItemList;
         }
+
+        private ValueDropdownList<int> _regionCfgIdList = new();
+
+        private const uint MAX_REGION_COUNT = 9;
+        private ValueDropdownList<int> GetValidRegions()
+        {
+            _regionCfgIdList.Clear();
+            _regionCfgIdList.Add(new ValueDropdownItem<int>($"未配置所属地段", 0));
+
+            for (int i = 1; i <= MAX_REGION_COUNT; i++)
+            {
+                _regionCfgIdList.Add(new ValueDropdownItem<int>($"地段ID:[{i}]", i));
+            }
+
+            return _regionCfgIdList;
+        }
+
+#endif
     }
 }
--- XProject/Assets/GameMain/Scripts/TripleMerge/MergeObject/MergeableCell.cs
+++ XProject/Assets/GameMain/Scripts/TripleMerge/MergeObject/MergeableCell.cs
@@ -21,25 +21,6 @@ namespace TripleMerge
         [LabelText("所属地段ID")] [ValueDropdown(nameof(GetValidRegions))]
 #endif
         public int BelongRegionId;
-#if UNITY_EDITOR
-
-        private ValueDropdownList<int> _regionCfgIdList = new();
-
-        private const uint MAX_REGION_COUNT = 9;
-        private ValueDropdownList<int> GetValidRegions()
-        {
-            _regionCfgIdList.Clear();
-            _regionCfgIdList.Add(new ValueDropdownItem<int>($"未配置所属地段", 0));
-
-            for (int i = 1; i <= MAX_REGION_COUNT; i++)
-            {
-                _regionCfgIdList.Add(new ValueDropdownItem<int>($"地段ID:[{i}]", i));
-            }
-
-            return _regionCfgIdList;
-        }
-
-#endif
         
         public enum ECellStatus
         {
