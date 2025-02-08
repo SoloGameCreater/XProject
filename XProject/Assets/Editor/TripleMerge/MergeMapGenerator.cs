aee1f6fc412f0f764757e3f99bed4404ab37fbea
dingzexingdragonplus <zexing.ding@dragonplus.com>
2025-02-08 15:41:51 +0800

    资源整理，地图生成工具修改

--- XProject/Assets/Editor/TripleMerge/MergeMapGenerator.cs
+++ XProject/Assets/Editor/TripleMerge/MergeMapGenerator.cs
@@ -20,14 +20,21 @@ namespace TripleMerge.Editor
         private const string CellPrefabPath = "Assets/ExtraRes/TripleMerge/Prefabs/MergeCell/MergeableCell.prefab";
         private GameObject _mergeCellPrefab;
 
+        private Sprite _cellA;
+        private Sprite _cellB;
+        
+        private const string CellAPath = "Assets/ExtraRes/TMatchRessss/World_Grass/grass_03_01.png";
+        private const string CellBPath = "Assets/ExtraRes/TMatchRessss/World_Grass/grass_04_01.png";
+
         public override void OnToolGUI(EditorWindow window)
         {
             _mapRoot = GameObject.Find("AreaRootEditor");
-            if(_mapRoot == null)
+            if (_mapRoot == null)
             {
                 _mapRoot = Instantiate(AssetDatabase.LoadAssetAtPath<GameObject>(EditorRootPrefabPath));
                 _mapRoot.name = "AreaRootEditor";
             }
+
             _cellRoot = _mapRoot?.transform.Find("LogicNode/MergeableRegion");
             _tilemap = _mapRoot?.transform.Find("Terrain/Grass").GetComponent<Tilemap>();
             _mergeCellPrefab = AssetDatabase.LoadAssetAtPath<GameObject>(CellPrefabPath);
@@ -57,7 +64,8 @@ namespace TripleMerge.Editor
             {
                 DestroyImmediate(_regionObj);
             }
-
+            _cellA = Instantiate(AssetDatabase.LoadAssetAtPath<Sprite>(CellAPath));
+            _cellB = Instantiate(AssetDatabase.LoadAssetAtPath<Sprite>(CellBPath));
             _regionObj = new GameObject("Region");
             _regionObj.transform.SetParent(_cellRoot);
             _regionObj.transform.Reset();
@@ -66,7 +74,7 @@ namespace TripleMerge.Editor
 
             // 获取地图grid信息
             BoundsInt bounds = _tilemap.cellBounds;
-
+            bool isCellA = true;
             for (int x = bounds.xMin; x < bounds.xMax; x++)
             {
                 for (int y = bounds.yMin; y < bounds.yMax; y++)
@@ -79,7 +87,8 @@ namespace TripleMerge.Editor
                         Vector3 worldPos = _tilemap.GetCellCenterWorld(cellPosition);
 
                         // 在对应位置创建cell
-                        CreateCell(worldPos, $"{x},{y}");
+                        CreateCell(worldPos, x, y,isCellA);
+                        isCellA = !isCellA;
                     }
                 }
             }
@@ -87,11 +96,15 @@ namespace TripleMerge.Editor
             return true;
         }
 
-        void CreateCell(Vector3 position, string text)
+        void CreateCell(Vector3 position, int x, int y, bool isCellA)
         {
-            GameObject cellObj = Instantiate(_mergeCellPrefab, position, Quaternion.identity, _regionObj.transform);
-            cellObj.AddComponent<MergeableCell>();
-            cellObj.name = $"Cell_[{text}]";
+            var cellObj = Instantiate(_mergeCellPrefab, position,
+                Quaternion.identity, _regionObj.transform);
+            
+            cellObj.name = $"Cell_[{x},{y}]";
+            cellObj.GetComponent<SpriteRenderer>().sprite = isCellA ? _cellA : _cellB;
+            var cell = cellObj.AddComponent<MergeableCell>();
+            cell.MapCoordinate = new Vector2Int(x, y);
         }
     }
 }
