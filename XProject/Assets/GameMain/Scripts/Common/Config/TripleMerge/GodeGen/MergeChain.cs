0126cc4448cea07abdf5a23bb2d982115d9faea5
dingzexing <burcetw@gmail.com>
2025-05-18 17:50:08 +0800

    合成链配置更新

--- XProject/Assets/GameMain/Scripts/Common/Config/TripleMerge/GodeGen/MergeChain.cs
+++ XProject/Assets/GameMain/Scripts/Common/Config/TripleMerge/GodeGen/MergeChain.cs
@@ -17,11 +17,6 @@ namespace Config.TripleMerge
         /// 合成链
         /// </summary>
         public List<int> Chain { get; set; }
-        /// <summary>
-        /// 在三合商城中出现的顺序
-        /// (0为第一个，不希望在商城中出现则配置为-1)
-        /// </summary>
-        public int ShopShowUpIndex { get; set; }
 
     }
 }
